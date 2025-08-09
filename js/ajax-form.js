a0199e7bf7c583154367943d135c9b19ef5d8180
Khajak-Hobosya <khajak.hobosyan@gmail.com>
2025-08-09 23:00:33 +0400

    form submition with google sheet

--- js/ajax-form.js
+++ js/ajax-form.js
@@ -1,48 +1,33 @@
-$(function() {
+﻿$(function () {
+    var form = $('#contact-form');
+    var formMessages = $('.ajax-response');
+    var submitBtn = form.find('button[type="submit"]');
 
-	// Get the form.
-	var form = $('#contact-form');
+    form.submit(function (e) {
+        e.preventDefault();
+        submitBtn.prop('disabled', true).text('Sending...');
 
-	// Get the messages div.
-	var formMessages = $('.ajax-response');
+        var formData = {
+            fullName: $('input[name="fullName"]').val(),
+            guestCount: $('select[name="guestCount"]').val(),
+            message: $('textarea[name="message"]').val()
+        };
 
-	// Set up an event listener for the contact form.
-	$(form).submit(function(e) {
-		// Stop the browser from submitting the form.
-		e.preventDefault();
-
-		// Serialize the form data.
-		var formData = $(form).serialize();
-
-		// Submit the form using AJAX.
-		$.ajax({
-			type: 'POST',
-			url: $(form).attr('action'),
-			data: formData
-		})
-		.done(function(response) {
-			// Make sure that the formMessages div has the 'success' class.
-			$(formMessages).removeClass('error');
-			$(formMessages).addClass('success');
-
-			// Set the message text.
-			$(formMessages).text(response);
-
-			// Clear the form.
-			$('#contact-form input,#contact-form textarea').val('');
-		})
-		.fail(function(data) {
-			// Make sure that the formMessages div has the 'error' class.
-			$(formMessages).removeClass('success');
-			$(formMessages).addClass('error');
-
-			// Set the message text.
-			if (data.responseText !== '') {
-				$(formMessages).text(data.responseText);
-			} else {
-				$(formMessages).text('Oops! An error occured and your message could not be sent.');
-			}
-		});
-	});
-
+        $.ajax({
+            url: 'YOUR_GOOGLE_SCRIPT_URL', // Paste your script URL here
+            type: 'POST',
+            contentType: 'application/json',
+            data: JSON.stringify(formData)
+        })
+            .done(function (response) {
+                formMessages.addClass('success').text(response);
+                form[0].reset();
+            })
+            .fail(function () {
+                formMessages.addClass('error').text("Error saving RSVP.");
+            })
+            .always(function () {
+                submitBtn.prop('disabled', false).text('Հաստատել');
+            });
+    });
 });
